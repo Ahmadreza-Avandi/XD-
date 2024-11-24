6b765f59d828f62c895f6d4fbb3f452585162208
Ahmadreza-Avandi <143386424+Ahmadreza-Avandi@users.noreply.github.com>
2024-11-24 13:35:01 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // پلاگین React
+  ],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // در صورتی که `lucide-react` نیاز به بهینه‌سازی نداشته باشد
+  },
+  build: {
+    rollupOptions: {
+      external: ['@tonconnect/sdk'], // ماژول `@tonconnect/sdk` را به عنوان external اضافه کنیم
+    },
+  },
+  resolve: {
+    alias: {
+      // استفاده از path.resolve برای حل مشکل در ESM
+      '@tonconnect/sdk': path.resolve(__dirname, 'node_modules/@tonconnect/sdk'),
+    },
   },
 });
+
